70a8829b48aedc9d2212415932c33e6c58ca0163
Anna Kilikova <103356373+klikli19@users.noreply.github.com>
2025-05-25 16:02:37 +0300

    1. ToMany [#301848]

---
+++ src/main/java/ru/job4j/cars/model/Post.java
@@ -0,0 +1,34 @@
+package ru.job4j.cars.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "auto_post")
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private int id;
+
+    private String description;
+
+    @Column(name = "created", nullable = false, updatable = false)
+    private LocalDateTime created = LocalDateTime.now();
+
+    @ManyToOne
+    @JoinColumn(name = "auto_user_id", nullable = false)
+    private User user;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "auto_post_id")
+    private List<PriceHistory> priceHistory = new ArrayList<>();
+
+}
---
+++ src/main/java/ru/job4j/cars/model/PriceHistory.java
@@ -0,0 +1,25 @@
+package ru.job4j.cars.model;
+
+import lombok.*;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "price_history")
+public class PriceHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private long beforePrice;
+
+    private long afterPrice;
+
+    @Column(name = "created", nullable = false, updatable = false)
+    private LocalDateTime created;
+
+}
